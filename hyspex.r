789323665367e714a3b1f44542f7b2864c961bb4
Ben Bradford <bbradford@wisc.edu>
2019-02-25 15:50:56 -0600

    Functionality for graphing the difference between mean spectral readings for AY NEG and POS

--- hyspex.r
+++ hyspex.r
@@ -8,44 +8,54 @@ dat =
   mutate(wav = round(as.numeric(wav),5)) %>%
   mutate_at(c("Mask", "Density", "Plot", "AY"),~ as.factor(.))
 
-gaps =
-  data.frame(wav = c(1.4, 1.9),
-             ref = c(NA, NA))
-
-
 spex20180822 = dat
 spex20180910 = dat
 
+spex =
+  rbind(
+    mutate(spex20180822, Date = 20180822),
+    mutate(spex20180910, Date = 20180910)
+  )
 
-p1 =
-  spex20180822 %>%
-  group_by(AY, wav) %>%
+spex_comp =
+  spex %>%
+  group_by(Date, AY, wav) %>%
   summarise(
     meanref = mean(ref),
     minref = min(ref),
     maxref = max(ref)
-  ) %>%
+  )
+
+spex_diff =
+  spex %>%
+  group_by(Date, AY, wav) %>%
+  summarise(meanref = mean(ref)) %>%
+  spread(AY, meanref) %>%
+  mutate(diff = log(NEG) - log(POS))
+
+
+p1 =
+  spex_comp %>%
   ggplot(aes(x = wav, y = meanref, color = AY)) +
+  facet_grid(Date ~ .) +
   geom_ribbon(aes(ymin = minref, ymax = maxref), alpha = .5) +
   geom_line() +
-  labs(title = "Spectral irradiance of aster-yellows infected carrot: 2018-08-22",
+  geom_hline(yintercept = 0) +
+  labs(title = "Spectral irradiance of aster-yellows infected carrot",
        x = "Wavelength",
        y = "Mean reflectance")
-CairoPNG("spex20180822.png", h=500, w=800); p1; dev.off()
+p1
+CairoPNG("spex20180822.png", h = 500, w = 800);p1;dev.off()
 
 
 p2 =
-  spex20180910 %>%
-  group_by(AY, wav) %>%
-  summarise(
-    meanref = mean(ref),
-    minref = min(ref),
-    maxref = max(ref)
-  ) %>%
-  ggplot(aes(x = wav, y = meanref, color = AY)) +
-  geom_ribbon(aes(ymin = minref, ymax = maxref), alpha = .5) +
-  geom_line() +
-  labs(title = "Spectral irradiance of aster-yellows infected carrot: 2018-09-10",
-       x = "Wavelength",
-       y = "Mean reflectance")
-CairoPNG("spex20180910.png", h=500, w=800); p2; dev.off()
+  spex_diff %>%
+  ggplot(aes(x = wav*1000)) +
+  facet_grid(Date ~ .) +
+  geom_hline(yintercept = 0) +
+  geom_ribbon(aes(ymin = 0, ymax = diff)) +
+  labs(x = "Wavelength",
+       y = "Log difference in means")
+p2
+
+  
