dcadd55917212a57586b2aeac01c2df97a08e672
Ben Bradford <bbradford@wisc.edu>
2019-04-05 11:16:47 -0500

    sim function

--- clust.r
+++ clust.r
@@ -1,45 +1,8 @@
 library(tidyverse)
 library(spatstat)
 
-# define WTM coordinate extents for carrot field
-fieldExtent =
-  data.frame(x = c(556040, 556107),
-             y = c(405070, 405119))
 
-# create ppp object from carrot locations and field extent
-carrot.ppp = carrots %>%
-  ppp(x = .$x,
-      y = .$y,
-      xrange = fieldExtent$x,
-      yrange = fieldExtent$y)
-
-# generate distmap for whole field
-carrot.distmap = distmap(carrot.ppp)
-png("distmaps/field.png")
-image(ay.distmap, main = "AY distmap")
-dev.off()
-
-# generate distmaps for each plot
-for (plot in levels(carrots$PlotID)) {
-  image =
-    carrots %>%
-    filter(PlotID == plot) %>%
-    ppp(x = .$x, y = .$y,
-        xrange = range(.$x) + c(-1, 1),
-        yrange = range(.$y) + c(-1, 1)) %>%
-    distmap()
-  png(paste0("distmaps/", plot, ".png"))
-  image(image, main = plot)
-  dev.off()
-}
-
-# monte carlo sims
-envelope(carrot.ppp, Kest, nsim = 39)
-
-
-
-#### random pts ####
-
+#### load and prep data ####
 clust_in =
   read.csv("in/carrot rel locs.csv") %>%
   mutate_if(is.character, as.factor) %>%
@@ -54,6 +17,8 @@ clust =
   mutate(Loc = NewLocDecFt) %>%
   select(c(Density, PlotID, Row, Loc))
 
+
+#### random carrot generation (non-ppp) ####
 # generate a number of random carrots matching number of AY carrots
 genpts =
   function(df, ...) {
@@ -67,34 +32,9 @@ genpts =
       ))
   }
 
-# same as above but returns a ppp object. Also doesn't take grouping vars
-genptsppp =
-  function(df) {
-    df %>%
-      do(data.frame(
-        Row = sample(1:3, nrow(.), replace = T),
-        Loc = runif(nrow(.), 0, 22),
-        n = nrow(.)
-      )) %>%
-      ppp(x = .$Row,
-          y = .$Loc,
-          xrange = c(1, 3),
-          yrange = c(0, 22))
-  }
-
+# randomize ay locs within plots
 rand_pts = genpts(clust, Density, PlotID)
-rand_pts_ppp = genptsppp(clust)
-
-
-genptsppp(clust)
-
-
-
-# generate random pts by plot
-clust %>%
-  group_by(PlotID) %>%
-  do(genpts(.))
-
+rand_pts
 
 # look at random pts
 rand_pts %>%
@@ -103,13 +43,30 @@ rand_pts %>%
   facet_wrap(~PlotID)
 
 
+#### random carrot generation (ppp) ####
+# same as above but returns a ppp object. Also doesn't take grouping vars
+genptsppp =
+  function(df,
+           xmin = 1,
+           xmax = 3,
+           ymin = 0,
+           ymax = 22) {
+    df %>%
+      do(data.frame(
+        Row = sample(xmin:xmax, nrow(.), replace = T),
+        Loc = runif(nrow(.), ymin, ymax),
+        n = nrow(.)
+      )) %>%
+      ppp(x = .$Row,
+          y = .$Loc,
+          xrange = c(xmin, xmax),
+          yrange = c(ymin, ymax))
+  }
 
-clust %>%
-  group_by(PlotID) %>%
-  ppp(x = .$Row, y = .$Loc, xrange = c(1, 3), yrange = c(0, 22)) %>%
-  envelope(., Kest, nsims = 10)
+randpts = genptsppp(clust)
 
 
+# clustering for whole field (don't use)
 clust.ppp =
   clust %>%
   ppp(x = .$Row,
@@ -121,6 +78,8 @@ clust.kenv = envelope(clust.ppp, Kest, nsims = 100)
 
 plot(clust.kenv)
 plot(clust.ppp)
+
+
 
 set.seed(422)
 
@@ -141,4 +100,53 @@ rand_env2 = envelope(rand2, Gest)
 
 plot(rand_env1$r ~ rand_env1$obs, type = "s")
 plot(rand_env2$r ~ rand_env2$obs, type = "s")
+
+
+levels(clust$PlotID)
+
+clust %>%
+  filter(PlotID == "H-102") %>%
+  genptsppp() %>%
+  plot()
+
+####
+for (p in levels(clust$PlotID)) {
+  print(p)
+}
+
+
+testplot =
+  clust %>%
+  filter(PlotID == levels(PlotID)[4])
+
+testppp =
+  testplot %>%
+  ppp(
+    x = .$Row,
+    y = .$Loc,
+    xrange = c(1, 3),
+    yrange = c(0, 22)
+  )
+
+for (s in 1:20) {
+  randppp = genptsppp(testplot)
+  env = envelope(randppp, Kest)
+  if (s == 1) {obs = NULL}
+  obs = cbind(obs, env$obs)
+}
+sim =
+  data.frame(
+  r = env$r,
+  obs = envelope(testppp, Kest)$obs,
+  min = apply(obs, 1, min),
+  max = apply(obs, 1, max)
+)
+sim %>%
+  ggplot(aes(x = r,
+             y = obs,
+             ymin = min,
+             ymax = max)) +
+  geom_area(alpha = .5) +
+  geom_line(color = "red")
+
 
